f1ad3b61ca54ce4354b31322350f3690e46524c7
Alyson Maruyama <alyson1907@gmail.com>
2024-12-07 17:18:49 -0300

    feat: add swagger-js-doc and documentation for CRUD endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,25 @@
 import express from "express";
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 import restaurantRouter from "./routes/restaurant.routes";
 
 const app = express();
 const port = 3000;
 
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Restaurant Orders API",
+      version: "1.0.0",
+      description: "Documentação da API do restaurante usando JSDoc",
+    },
+  },
+  apis: ["./src/routes/*.ts"], // Localização dos arquivos com JSDoc
+};
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
 app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use("/api", restaurantRouter);
 
 app.listen(port, () => {
