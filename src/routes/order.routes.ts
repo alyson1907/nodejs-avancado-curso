bfcc6e484248c1446c797415f526d2a63613a5ec
Alyson Maruyama <alyson1907@gmail.com>
2025-01-14 18:26:57 -0300

    feat: Add Swagger documentation for module 4 endpoints

--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -7,8 +7,266 @@ import {
 
 const router = Router();
 
+/**
+ * @swagger
+ * /restaurant/{id}/orders:
+ *   post:
+ *     summary: Cria um novo pedido para um restaurante específico
+ *     tags: [Pedidos]
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         description: ID do restaurante onde o pedido será criado
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               dishes:
+ *                 type: array
+ *                 items:
+ *                   type: object
+ *                   properties:
+ *                     dishName:
+ *                       type: string
+ *                       description: Nome do prato
+ *                     amount:
+ *                       type: integer
+ *                       description: Quantidade do prato
+ *                     price:
+ *                       type: number
+ *                       format: float
+ *                       description: Preço do prato
+ *     responses:
+ *       200:
+ *         description: Pedido criado com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: string
+ *                       description: ID do pedido
+ *                     restaurantId:
+ *                       type: string
+ *                       description: ID do restaurante associado ao pedido
+ *                     status:
+ *                       type: string
+ *                       description: Status do pedido
+ *                       example: CREATED
+ *                     createdAt:
+ *                       type: string
+ *                       format: date-time
+ *                       description: Data de criação do pedido
+ *                     updatedAt:
+ *                       type: string
+ *                       format: date-time
+ *                       description: Data da última atualização do pedido
+ *                     dishes:
+ *                       type: array
+ *                       items:
+ *                         type: object
+ *                         properties:
+ *                           id:
+ *                             type: string
+ *                             description: ID do prato no pedido
+ *                           orderId:
+ *                             type: string
+ *                             description: ID do pedido associado ao prato
+ *                           dishName:
+ *                             type: string
+ *                             description: Nome do prato
+ *                           amount:
+ *                             type: integer
+ *                             description: Quantidade do prato
+ *                           price:
+ *                             type: number
+ *                             format: float
+ *                             description: Preço do prato
+ *                           createdAt:
+ *                             type: string
+ *                             format: date-time
+ *                             description: Data de criação do prato
+ *                           updatedAt:
+ *                             type: string
+ *                             format: date-time
+ *                             description: Data da última atualização do prato
+ */
 router.post("/restaurant/:id/orders", createRestaurantOrder);
+/**
+ * @swagger
+ * /orders:
+ *   get:
+ *     summary: Obtém todos os pedidos existentes
+ *     tags: [Pedidos]
+ *     parameters:
+ *       - name: restaurantId
+ *         in: query
+ *         required: false
+ *         description: Filtra pedidos pelo ID do restaurante
+ *         schema:
+ *           type: string
+ *       - name: status
+ *         in: query
+ *         required: false
+ *         description: Filtra pedidos pelo status
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Lista de pedidos obtida com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: string
+ *                         description: ID do pedido
+ *                       restaurantId:
+ *                         type: string
+ *                         description: ID do restaurante associado ao pedido
+ *                       status:
+ *                         type: string
+ *                         description: Status do pedido
+ *                         example: CREATED
+ *                       createdAt:
+ *                         type: string
+ *                         format: date-time
+ *                         description: Data de criação do pedido
+ *                       updatedAt:
+ *                         type: string
+ *                         format: date-time
+ *                         description: Data da última atualização do pedido
+ *                       dishes:
+ *                         type: array
+ *                         items:
+ *                           type: object
+ *                           properties:
+ *                             id:
+ *                               type: string
+ *                               description: ID do prato no pedido
+ *                             orderId:
+ *                               type: string
+ *                               description: ID do pedido associado ao prato
+ *                             dishName:
+ *                               type: string
+ *                               description: Nome do prato
+ *                             amount:
+ *                               type: integer
+ *                               description: Quantidade do prato
+ *                             price:
+ *                               type: number
+ *                               format: float
+ *                               description: Preço do prato
+ *                             createdAt:
+ *                               type: string
+ *                               format: date-time
+ *                               description: Data de criação do prato
+ *                             updatedAt:
+ *                               type: string
+ *                               format: date-time
+ *                               description: Data da última atualização do prato
+ */
 router.get("/orders", getOrder);
+/**
+ * @swagger
+ * /orders/{orderId}:
+ *   patch:
+ *     summary: Atualiza o status de um pedido específico
+ *     tags: [Pedidos]
+ *     parameters:
+ *       - name: orderId
+ *         in: path
+ *         required: true
+ *         description: ID do pedido a ser atualizado
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       description: Dados para atualizar o status do pedido
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               status:
+ *                 type: string
+ *                 description: Novo status do pedido
+ *                 example: IN_PROGRESS
+ *     responses:
+ *       200:
+ *         description: Pedido atualizado com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: string
+ *                       description: ID do pedido atualizado
+ *                     restaurantId:
+ *                       type: string
+ *                       description: ID do restaurante associado ao pedido
+ *                     status:
+ *                       type: string
+ *                       description: Novo status do pedido
+ *                       example: IN_PROGRESS
+ *                     createdAt:
+ *                       type: string
+ *                       format: date-time
+ *                       description: Data de criação do pedido
+ *                     updatedAt:
+ *                       type: string
+ *                       format: date-time
+ *                       description: Data da última atualização do pedido
+ *                     dishes:
+ *                       type: array
+ *                       items:
+ *                         type: object
+ *                         properties:
+ *                           id:
+ *                             type: string
+ *                             description: ID do prato no pedido
+ *                           orderId:
+ *                             type: string
+ *                             description: ID do pedido associado ao prato
+ *                           dishName:
+ *                             type: string
+ *                             description: Nome do prato
+ *                           amount:
+ *                             type: integer
+ *                             description: Quantidade do prato
+ *                           price:
+ *                             type: number
+ *                             format: float
+ *                             description: Preço do prato
+ *                           createdAt:
+ *                             type: string
+ *                             format: date-time
+ *                             description: Data de criação do prato
+ *                           updatedAt:
+ *                             type: string
+ *                             format: date-time
+ *                             description: Data da última atualização do prato
+ */
 router.patch("/orders/:orderId", updateOrder);
 
 export default router;
--- src/routes/restaurant.routes.ts
+++ src/routes/restaurant.routes.ts
@@ -8,8 +8,224 @@ import {
 
 const router = Router();
 
+/**
+ * @swagger
+ * /restaurant:
+ *   post:
+ *     summary: Cria um novo restaurante
+ *     tags: [Restaurantes]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 description: Nome do restaurante
+ *               description:
+ *                 type: string
+ *                 nullable: true
+ *                 description: Descrição do restaurante
+ *             required:
+ *               - name
+ *     responses:
+ *       201:
+ *         description: Restaurante criado com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: string
+ *                       description: ID único do restaurante criado
+ *                     name:
+ *                       type: string
+ *                       description: Nome do restaurante
+ *                     description:
+ *                       type: string
+ *                       nullable: true
+ *                       description: Descrição do restaurante
+ *                     isDeleted:
+ *                       type: boolean
+ *                       description: Indica se o restaurante está marcado como deletado
+ *                     createdAt:
+ *                       type: string
+ *                       format: date-time
+ *                       description: Data de criação do restaurante
+ *                     updatedAt:
+ *                       type: string
+ *                       format: date-time
+ *                       description: Data da última atualização do restaurante
+ */
 router.post("/restaurant", createRestaurant);
+/**
+ * @swagger
+ * /restaurant:
+ *   get:
+ *     summary: Lista restaurantes com base em filtros opcionais
+ *     tags: [Restaurantes]
+ *     parameters:
+ *       - name: name
+ *         in: query
+ *         required: false
+ *         description: Nome do restaurante para filtrar
+ *         schema:
+ *           type: string
+ *       - name: description
+ *         in: query
+ *         required: false
+ *         description: Descrição do restaurante para filtrar
+ *         schema:
+ *           type: string
+ *       - name: isDeleted
+ *         in: query
+ *         required: false
+ *         description: Filtra restaurantes marcados como deletados
+ *         schema:
+ *           type: boolean
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 description: Nome do restaurante
+ *               description:
+ *                 type: string
+ *                 nullable: true
+ *                 description: Descrição do restaurante
+ *     responses:
+ *       200:
+ *         description: Lista de restaurantes retornada com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: string
+ *                         description: ID único do restaurante
+ *                       name:
+ *                         type: string
+ *                         description: Nome do restaurante
+ *                       description:
+ *                         type: string
+ *                         nullable: true
+ *                         description: Descrição do restaurante
+ *                       isDeleted:
+ *                         type: boolean
+ *                         description: Indica se o restaurante está marcado como deletado
+ *                       createdAt:
+ *                         type: string
+ *                         format: date-time
+ *                         description: Data de criação do restaurante
+ *                       updatedAt:
+ *                         type: string
+ *                         format: date-time
+ *                         description: Data da última atualização do restaurante
+ */
 router.get("/restaurant", getRestaurants);
+/**
+ * @swagger
+ * /restaurant/{id}:
+ *   patch:
+ *     summary: Atualiza as informações de um restaurante específico
+ *     tags: [Restaurantes]
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         description: ID do restaurante a ser atualizado
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               description:
+ *                 type: string
+ *                 description: Nova descrição do restaurante
+ *     responses:
+ *       200:
+ *         description: Restaurante atualizado com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: string
+ *                       description: ID único do restaurante
+ *                     name:
+ *                       type: string
+ *                       description: Nome do restaurante
+ *                     description:
+ *                       type: string
+ *                       description: Descrição atualizada do restaurante
+ *                     isDeleted:
+ *                       type: boolean
+ *                       description: Indica se o restaurante está marcado como deletado
+ *                     createdAt:
+ *                       type: string
+ *                       format: date-time
+ *                       description: Data de criação do restaurante
+ *                     updatedAt:
+ *                       type: string
+ *                       format: date-time
+ *                       description: Data da última atualização do restaurante
+ */
 router.patch("/restaurant/:id", updateRestaurant);
+/**
+ * @swagger
+ * /restaurant/{id}:
+ *   delete:
+ *     summary: Remove um restaurante específico
+ *     tags: [Restaurantes]
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         description: ID do restaurante a ser removido
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: false
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               description:
+ *                 type: string
+ *                 description: Descrição adicional (não necessária para exclusão)
+ *     responses:
+ *       200:
+ *         description: Restaurante removido com sucesso
+ *         content:
+ *           text/plain:
+ *             schema:
+ *               type: string
+ *               example: Restaurant 67805e32b411c6d255fec757 was removed
+ */
 router.delete("/restaurant/:id", deleteRestaurant);
 export default router;
